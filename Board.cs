f6c500d5475a214b65475bdc10188758bd3eb044
Ian Blackmore <Iblackmore4899@conestogac.on.ca>
2026-04-29 11:08:46 -0400

    fixed bugs

--- Board.cs
+++ Board.cs
@@ -151,9 +151,9 @@ public partial class Board : Control
 	public string serializeState(byte[,] grid){
 		char[] flattenedState = new char[20];
 		int ind = 0;
-		for(int i = 0; i < 5; i++){
-			for(int i = 0; i < 5; i++){
-			 	flattendState = (char)grid[x,y];
+		for(int x = 0; x < 5; x++){
+			for(int y = 0; y < 5; y++){
+			 	flattenedState = (char)grid[x,y];
 			}
 		}
 		return new string(flattenedState);
